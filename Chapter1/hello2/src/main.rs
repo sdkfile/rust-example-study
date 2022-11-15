739866b7e3a32d9bd389a969198a4684d1c55650
Seonggu Kim <seonggu.de.kim@gmail.com>
2022-11-15 22:18:39 +0900

    initial commit

---
+++ Chapter1/hello2/src/main.rs
@@ -0,0 +1,13 @@
+fn greet_world() {
+    println!("Hello, world!");
+    let southern_germany = "Grüß Gott!";
+    let korean = "안녕 세상아!";
+    let regions = [southern_germany, korean];
+    for region in regions.iter() {
+        println!("{}", &region);
+    }
+}
+
+fn main() {
+    greet_world();
+}
