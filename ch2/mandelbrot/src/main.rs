e95e557a9ea01024b7a1afadc40846770af503be
Seonggu Kim <seonggu.de.kim@gmail.com>
2022-11-18 17:51:11 +0900

    chapter 2 done

--- ch2/mandelbrot/src/main.rs
+++ ch2/mandelbrot/src/main.rs
@@ -9,13 +9,12 @@ fn calculate_mandelbrot(
     width: usize,
     height: usize,
 ) -> Vec<Vec<usize>> {
-    let mut rows: Vec<_> = Vec:with_capacity(width);
+    let mut rows: Vec<_> = Vec::with_capacity(width);
     for img_y in 0..height {
         let mut row: Vec<usize> = Vec::with_capacity(height);
         for img_x in 0..width {
-
-            let x_percent = (img_x as f64 / width as f64);
-            let y_percent = (img_y as f64 / height as f64);
+            let x_percent = img_x as f64 / width as f64;
+            let y_percent = img_y as f64 / height as f64;
             let cx = x_min + (x_max - x_min) * x_percent;
             let cy = y_min + (y_max - y_min) * y_percent;
             let escaped_at = mandelbrot_at_point(cx, cy, max_iters);
@@ -36,7 +35,7 @@ fn mandelbrot_at_point(
 
     for i in 0..=max_iters {
         if z.norm() > 2.0 {
-            return i
+            return i;
         }
         z = z * z + c;
     }
@@ -45,24 +44,25 @@ fn mandelbrot_at_point(
 
 fn render_mandelbrot(escape_vals: Vec<Vec<usize>>) {
     for row in escape_vals {
-        let mut line = String:with_capacity(row.len());
+        let mut line = String::with_capacity(row.len());
         for column in row {
             let val = match column {
-                0..=2 => " ",
-                3..=5 => ".",
-                6..=10 => "`",
-                11..=30 => "*",
-                31..=100 => "+",
-                101..=200 => "x",
-                201..=400 => "$",
-                401..=700 => "#",
-                _ => "%",
+                0..=2 => ' ',
+                3..=5 => '.',
+                6..=10 => '•',
+                11..=30 => '*',
+                31..=100 => '+',
+                101..=200 => 'x',
+                201..=400 => '$',
+                401..=700 => '#',
+                _ => '%',
             };
             line.push(val);
         }
-        println("{}", line);
+        println!("{}", line);
     }
 }
+
 fn main() {
     let mandelbrot = calculate_mandelbrot(1000, -2.0, 1.0, -1.0, 1.0, 100, 24);
     render_mandelbrot(mandelbrot);
