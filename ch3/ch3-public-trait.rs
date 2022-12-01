3c33d8d52d0ed00ab1300d8fc9aefe275e832e11
Seonggu Kim <seonggu.de.kim@gmail.com>
2022-12-01 21:10:13 +0900

    chapter 3 done

---
+++ ch3/ch3-public-trait.rs
@@ -0,0 +1,27 @@
+#[derive(Debug, PartialEq)]
+pub enum FileState {
+    Open,
+    Closed,
+}
+
+#[derive(Debug)]
+pub struct File {
+    name: String,
+    data: Vec<u8>,
+    state: FileState,
+}
+
+impl File {
+    pub fn new(name: &str) -> File {
+        File {
+            name: String::from(name),
+            data: Vec::new(),
+            state: FileState::Closed,
+        }
+    }
+}
+
+fn main() {
+    let f7 = File::new("7.txt");
+    println!("{:?}", f7);
+}
